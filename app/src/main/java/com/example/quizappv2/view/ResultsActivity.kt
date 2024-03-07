15c807f69109578090142e652bdf8d57d0d91062
Nam <namd19ptit@gmail.com>
2024-03-07 23:18:06 +0700

    reasult Activity

--- app/src/main/java/com/example/quizappv2/view/ResultsActivity.kt
+++ app/src/main/java/com/example/quizappv2/view/ResultsActivity.kt
@@ -1,12 +1,24 @@
 package com.example.quizappv2.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
 import com.example.quizappv2.R
+import com.example.quizappv2.databinding.ActivityQuizBinding
+import com.example.quizappv2.databinding.ActivityResultsBinding
 
 class ResultsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityResultsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_results)
+        binding = ActivityResultsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.txtAnswer.text = "Your Score is: "+QuizActivity.result +"/"+QuizActivity.totalQuestion
+        binding.btnBack.setOnClickListener {
+            var intent = Intent(this@ResultsActivity, QuizActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
